70829e89bc20e2366cc0755d49a7051fb969e04e
Puneet56 <puneetdhiman56@gmail.com>
2023-12-08 15:44:02 +0530

    init

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/test", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, test World!")
+	})
+
+	e.GET("/users", getUsers)
+	e.GET("/users/:id", getUser)
+	e.POST("/users", createUser)
+	e.PUT("/users", editUser)
+
+	log.Fatal(e.Start(":6969"))
+}
+
+type User struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+var users = make([]User, 0)
+
+func getUsers(c echo.Context) error {
+	return c.JSON(http.StatusOK, users)
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	for _, user := range users {
+		if user.ID == id {
+			return c.JSON(http.StatusOK, user)
+		}
+	}
+
+	return c.JSON(http.StatusNotFound, nil)
+}
+
+func createUser(c echo.Context) error {
+	user := User{}
+	if err := c.Bind(&user); err != nil {
+		return err
+	}
+	user.ID = uuid.NewString()
+	users = append(users, user)
+	return c.JSON(http.StatusCreated, user)
+}
+
+func editUser(c echo.Context) error {
+	user := User{}
+	if err := c.Bind(&user); err != nil {
+		return err
+	}
+
+	for _, u := range users {
+		if u.ID == user.ID {
+			u.Name = user.Name
+			u.Email = user.Email
+
+			return c.JSON(http.StatusOK, u)
+		}
+	}
+
+	return c.JSON(http.StatusNotFound, "User not found"+user.ID)
+}
